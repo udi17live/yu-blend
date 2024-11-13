37acfbaba98e69b7bc8f9b955c3202ca7ec488d3
Uditha Mahindarathna <melan.udi@gmail.com>
2024-11-13 18:28:42 +0530

    initial commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import * as React from "react";
+
+export default function Home() {
+  return <div>Home Page</div>;
+}
---
+++ frontend/src/components/layout/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import { ChevronDown, Home, Search } from "lucide-react";
+
+export const Header = () => {
+  return <div>Header</div>;
+};
